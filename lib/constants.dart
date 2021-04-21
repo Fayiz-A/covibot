1a98d1d0f2bda6bb01d8c909da083b307cf2e969
Fayiz-A <68380206+Fayiz-A@users.noreply.github.com>
2021-04-22 00:09:49 +0530

    added circle progress indicator for loading purposes

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,4 @@
 const String emptyStringQuery = 'nothing in query with passcode abc@77';
 const String initialMessageFromChatbot = 'Hi, how may I assist you?';
 const Duration messageDurationForCornerCaseMessages = Duration(milliseconds: 1500);
+const Duration durationBeforeRenderingProgressIndicator = Duration(milliseconds: 150);
