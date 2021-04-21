78824340110af3933f7ce9e0e7b545fdf1094783
Fayiz-A <68380206+Fayiz-A@users.noreply.github.com>
2021-04-22 00:34:07 +0530

    added animatedCrossFade between progress indicator and message. Addedanimated spin kit

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,3 +2,4 @@ const String emptyStringQuery = 'nothing in query with passcode abc@77';
 const String initialMessageFromChatbot = 'Hi, how may I assist you?';
 const Duration messageDurationForCornerCaseMessages = Duration(milliseconds: 1500);
 const Duration durationBeforeRenderingProgressIndicator = Duration(milliseconds: 150);
+const Duration fadeDurationBetweenProgressIndicatorAndMessage = Duration(milliseconds: 200);
