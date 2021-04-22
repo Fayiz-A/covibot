8efe41565471c3b6ff115e27f74957fa4d71bf72
Fayiz-A <68380206+Fayiz-A@users.noreply.github.com>
2021-04-23 01:00:24 +0530

    created settings bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:covibot/blocs/chatbot_bloc.dart';
 import 'package:covibot/blocs/internet_connnection_bloc.dart';
+import 'package:covibot/blocs/settings_bloc.dart';
 import 'package:covibot/constants.dart' as constants;
 import 'package:covibot/screens/chatbot_page.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_dialogflow/dialogflow_v2.dart';
+
+import 'screens/settings_page.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,28 +25,10 @@ Future<void> main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     Locale locale = context.locale;
     print(locale);
-    return MaterialApp(
-      title: 'CoviBot',
-      localizationsDelegates: context.localizationDelegates,
-      supportedLocales: context.supportedLocales,
-      locale: context.locale,
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-          primaryColor: Colors.red,
-          splashFactory: InkRipple.splashFactory,
-          textTheme: TextTheme(
-            bodyText1: TextStyle(fontSize: 20),
-          ),
-          tooltipTheme: TooltipThemeData(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(20.0)),
-              color: Colors.black.withOpacity(0.6),
-            ),
-          )),
-      home: MultiBlocProvider(providers: [
+    return MultiBlocProvider(
+      providers: [
         BlocProvider(
           lazy: false,
           create: (BuildContext context) => ChatbotBloc()
@@ -53,9 +37,34 @@ class MyApp extends StatelessWidget {
                 message: constants.initialMessageFromChatbot)),
         ),
         BlocProvider(
-          create: (BuildContext context) => InternetConnectionBloc(),
+            create: (BuildContext context) => SettingsBloc(),
         ),
-      ], child: SafeArea(top: false, child: ChatbotPage())),
+      ],
+      child: BlocBuilder<SettingsBloc, SettingsState>(
+        builder: (BuildContext context, SettingsState state) {
+          print(state.themeData.brightness);
+          return MaterialApp(
+            title: 'CoviBot',
+            localizationsDelegates: context.localizationDelegates,
+            supportedLocales: context.supportedLocales,
+            locale: context.locale,
+            debugShowCheckedModeBanner: false,
+            theme: state.themeData,
+            home: MultiBlocProvider(providers: [
+              BlocProvider(
+                lazy: false,
+                create: (BuildContext context) => ChatbotBloc()
+                  ..chatbotLocale = locale
+                  ..add(SendMessageFromChatbotEvent(
+                      message: constants.initialMessageFromChatbot)),
+              ),
+              BlocProvider(
+                create: (BuildContext context) => InternetConnectionBloc(),
+              ),
+            ], child: SafeArea(top: false, child: ChatbotPage())),
+          );
+        }
+      ),
     );
   }
 }
