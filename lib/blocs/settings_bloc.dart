4f0f1da35b49d5e4d4841ba7044997086efba232
Fayiz-A <68380206+Fayiz-A@users.noreply.github.com>
2021-04-23 22:19:48 +0530

    added settings initial option

--- lib/blocs/settings_bloc.dart
+++ lib/blocs/settings_bloc.dart
@@ -27,7 +27,7 @@ class SetInitialValuesEvent extends SettingsEvent {
 
 
 class SettingsBloc extends Bloc<SettingsEvent, SettingsState> {
-  SettingsBloc() : super(ThemeChangedState(themeMode: ThemeMode.light, fontSize: 20.0));
+  SettingsBloc() : super(ThemeChangedState(themeMode: ThemeMode.light, fontSize: 20.0, locale: Locale('en', 'UK')));
 
   ThemeMode themeMode = ThemeMode.dark;
   double fontSize = 20.0;
@@ -41,19 +41,25 @@ class SettingsBloc extends Bloc<SettingsEvent, SettingsState> {
       fontSize = event.fontSize;
       locale = event.locale;
 
+
+      yield InitialsSetState(themeMode: themeMode, fontSize: fontSize, locale: locale);
+
     } else if (event is ToggleThemeEvent) {
 
       themeMode = themeMode == ThemeMode.light ? ThemeMode.dark:ThemeMode.light;
+
+      yield ThemeChangedState(themeMode: themeMode, fontSize: fontSize, locale: locale);
 
     } else if(event is ChangeFontStyleEvent) {
 
       fontSize = event.fontSize;
 
+      yield ThemeChangedState(themeMode: themeMode, fontSize: fontSize, locale: locale);
+
     } else if(event is ChangeLanguageEvent) {
       //do something
     }
 
-    yield ThemeChangedState(themeMode: themeMode, fontSize: fontSize);
   }
 }
 
@@ -67,7 +73,17 @@ abstract class SettingsState {
 class ThemeChangedState extends SettingsState {
   final ThemeMode themeMode;
   final double fontSize;
+  final Locale locale;
 
-  ThemeChangedState({@required this.themeMode, @required this.fontSize}) :super(themeMode, fontSize);
+  ThemeChangedState({@required this.themeMode, @required this.fontSize, @required this.locale,}) :super(themeMode, fontSize);
+
+}
+
+class InitialsSetState extends SettingsState {
+  final ThemeMode themeMode;
+  final double fontSize;
+  final Locale locale;
+
+  InitialsSetState({@required this.themeMode, @required this.fontSize, @required this.locale,}) :super(themeMode, fontSize);
 
 }
